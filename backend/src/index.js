5238aac123808691ad67792a7640f6e5b4a64fc4
BrianAhuga <ahuga2002@gmail.com>
2026-07-25 16:28:13 +0300

    Add webhook endpoint for Clerk to handle raw event data

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,8 +11,8 @@ import { clerkMiddleware } from "@clerk/express";
 
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
-
 import job from "./lib/cron.js";
+import clerkWebhook from "./webhooks/clerk.webhook.js";
 
 const app = express();
 
@@ -20,6 +20,13 @@ const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const publicDir = path.join(process.cwd(), "public");
+
+// it's important not to parse the webhook event data, it should be in raw format
+app.use(
+  "/api/webhooks/clerk",
+  express.raw({ type: "application/json" }),
+  clerkWebhook,
+);
 
 app.use(express.json());
 app.use(cors({ origin: FRONTEND_URL, credentials: true }));
