065b1517b4cde1cfb74f0c4f4d59afd1e72dbd4e
BrianAhuga <ahuga2002@gmail.com>
2026-07-26 18:34:55 +0300

    Implement socket.io for real-time messaging; add socket connection handling and user management

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../lib/socket.js";
 
 export async function getUsersForSidebar(req, res) {
   try {
@@ -121,6 +122,11 @@ export async function sendMessage(req, res) {
     await newMessage.save();
 
     // todo: realtime with socketio
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    // only send the message in realtime if user is online
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
 
     res.status(201).json(newMessage);
   } catch (error) {
